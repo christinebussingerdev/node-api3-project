d02680226bf495e955fc3f913f34c80d07385399
Christine Bussinger <christinebussingerdev@gmail.com>
2020-04-16 19:35:32 -0400

    basic setup

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
 const express = require('express');
 
+// router imports
+const postRouter = require('./posts/postRouter')
+const userRouter = require('./users/userRouter')
+
+// init server
 const server = express();
+server.use(express.json())
+
+// routers
+server.use('/posts', postRouter)
+server.use('/users', userRouter)
+
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
-
+// custom middleware
 function logger(req, res, next) {}
 
 module.exports = server;
